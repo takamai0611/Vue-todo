043b6256b54b025b4afd1a31110e6274ece63817
高橋舞 <tkjnpamw5611@gmail.com>
2023-03-25 20:47:50 +0900

    delete:不要な記述を消去

--- src/store/todo/index.ts
+++ src/store/todo/index.ts
@@ -5,27 +5,7 @@ import { Todo, TodoState, TodoStore, Params } from '@/store/todo/types'
 const mockTodo: Todo[] = [
   {
     id: 1,
-    title: 'todo1',
-    description: '1つ目',
-    status: 'waiting',
-    createdAt: new Date('2020-12-01'),
-    updatedAt: new Date('2020-12-01')
-  },
-  {
-    id: 2,
-    title: 'todo2',
-    description: '2つ目',
-    status: 'waiting',
-    createdAt: new Date('2020-12-02'),
-    updatedAt: new Date('2020-12-02')
-  },
-  {
-    id: 3,
-    title: 'todo3',
-    description: '3つ目',
-    status: 'working',
-    createdAt: new Date('2020-12-03'),
-    updatedAt: new Date('2020-12-04')
+    title: 'todo1'
   }
 ]
 
