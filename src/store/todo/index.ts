2a2e04d025366c56815c99d3bc014cec64210e10
高橋舞 <tkjnpamw5611@gmail.com>
2023-03-25 14:39:48 +0900

    add:ストアの実装

---
+++ src/store/todo/index.ts
@@ -0,0 +1,89 @@
+import { InjectionKey, reactive, readonly } from 'vue'
+import { Todo, TodoState, TodoStore } from '@/store/todo/types'
+
+// ①
+const mockTodo: Todo[] = [
+  {
+    id: 1,
+    title: 'todo1',
+    description: '1つ目',
+    status: 'waiting',
+    createdAt: new Date('2020-12-01'),
+    updatedAt: new Date('2020-12-01'),
+  },
+  {
+    id: 2,
+    title: 'todo2',
+    description: '2つ目',
+    status: 'waiting',
+    createdAt: new Date('2020-12-02'),
+    updatedAt: new Date('2020-12-02'),
+  },
+  {
+    id: 3,
+    title: 'todo3',
+    description: '3つ目',
+    status: 'working',
+    createdAt: new Date('2020-12-03'),
+    updatedAt: new Date('2020-12-04'),
+  },
+]
+
+// ②
+const state = reactive<TodoState>({
+  todos: mockTodo,
+})
+
+// ③
+const intitializeTodo = (todo: Params) => {
+    const date = new Date()
+    return {
+      id: date.getTime(),
+      title: todo.title,
+      description: todo.description,
+      status: todo.status,
+      createdAt: date,
+      updatedAt: date,
+    } as Todo
+  }
+
+// ④
+const getTodo = (id: number) => {
+  const todo = state.todos.find((todo) => todo.id === id)
+  if (!todo) {
+    throw new Error(`cannot find todo by id:${id}`)
+  }
+  return todo
+}
+
+// ⑤
+const addTodo = (todo: Params) => {
+  state.todos.push(intitializeTodo(todo))
+}
+
+// ⑥
+const updateTodo = (id: number, todo: Todo) => {
+  const index = state.todos.findIndex((todo) => todo.id === id)
+  if (index === -1) {
+    throw new Error(`cannot find todo by id:${id}`)
+  }
+  state.todos[index] = todo
+}
+
+// ⑦
+const deleteTodo = (id: number) => {
+  state.todos = state.todos.filter((todo) => todo.id !== id)
+}
+
+const todoStore: TodoStore = {
+  state: readonly(state),
+  getTodo,
+  addTodo,
+  updateTodo,
+  deleteTodo,
+}
+
+export default todoStore
+
+// ⑧
+export const todoKey: InjectionKey<TodoStore> = Symbol('todo')
